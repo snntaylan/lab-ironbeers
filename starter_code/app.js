bf2fed04a445b914e19f00439639f5ca65a42a81
snntaylan <sinan.taylan@outlook.com>
2019-02-03 21:54:47 +0100

    updated

--- starter_code/app.js
+++ starter_code/app.js
@@ -2,37 +2,63 @@
 const express = require('express');
 const hbs     = require('hbs');
 const app     = express();
+
 const path    = require('path');
 const PunkAPIWrapper = require('punkapi-javascript-wrapper');
 const punkAPI = new PunkAPIWrapper();
 
-hbs.registerPartials(__dirname + '/views/partials')
+// hbs.registerPartials(__dirname + '/views/partials')
 
+// Set the view engine to be HBS with views in the `/views` folder
 app.set('view engine', 'hbs');
 app.set('views', __dirname + '/views');
+
+// Everything inside `/public is accessible`
+/**
+ * path.join returns a normalized path 
+ * by merging two paths together. 
+ * It can return an absolute path, 
+ * but it doesn't necessarily always do so.
+ * 
+ */
 app.use(express.static(path.join(__dirname, 'public')));
+
+/**
+ * Using __dirname is the absolute path to the directory containing the source file. 
+ * When you use path.resolve or path.join 
+ * they will return the same result if you give the same path following __dirname
+ */
+
+// console.log(__dirname)
+
+
+// Route: GET /
 
 app.get('/', (req, res, next) => {
   res.render('index');
 });
-app.get('/beers', (req, res, next) => {
+
+
+app.get('/beers', (req, res, next) => { 
   punkAPI.getBeers()
-  .then(beers => {
+  .then(beers => { // beers is an array of objects
     res.render('beers', {beers});
   })
   .catch(error => {
     console.log(error)
   })
 });
-app.get('/random-beers', (req, res, next) => {
-  punkAPI.getRandom()
-  .then(randomBeers => {
-    res.render('random-beers', {randomBeers});
-  })
+
+app.get('/random-beer', (req, res, next) => {
+  punkAPI.getRandom() // promises
+  .then(beers => { // // beers is an array with 1 object (yes,it's stupid)
+    console.log("DEBUG x",)
+    res.render('random-beer', {
+    beer: beers[0]
+  });
+})
   .catch(error => {
     console.log(error)
   })
 });
-
-
 app.listen(3000);
